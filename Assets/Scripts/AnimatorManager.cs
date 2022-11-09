88a34ff25c3e88cc255cb9a4b429da1f194aaea1
sundev-nalees <72244488+sundev-nalees@users.noreply.github.com>
2022-11-09 11:27:11 +0530

    PlayerAnimation-walkandRun

---
+++ Assets/Scripts/AnimatorManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour
+{
+    Animator animator;
+    private int horizontal;
+    private int vertical;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        horizontal = Animator.StringToHash("Horizontal");
+        vertical = Animator.StringToHash("Vertical");
+    }
+
+    public void UpdateAnimatorValues(float horizontalMovement,float verticalMovement)
+    {
+
+        //Animation Snaping
+        float snappedHorizontal;
+        float snappedVertical;
+
+        #region Snapeed Horizontal
+        if (horizontalMovement > 0 && horizontalMovement < 0.55f)
+        {
+            snappedHorizontal = 0.5f;
+        }
+        else if (horizontalMovement > 0.55f)
+        {
+            snappedHorizontal = 1;
+        }
+        else if (horizontalMovement < 0 && horizontalMovement > -0.55f)
+        {
+            snappedHorizontal = -0.5f;
+        }
+        else if (horizontalMovement < -0.5f)
+        {
+            snappedHorizontal = -1;
+        }
+        else
+        {
+            snappedHorizontal = 0;
+        }
+        #endregion
+
+        #region Snapeed Vertical
+        if (verticalMovement > 0 && verticalMovement < 0.55f)
+        {
+            snappedVertical = 0.5f;
+        }
+        else if (verticalMovement > 0.55f)
+        {
+            snappedVertical = 1;
+        }
+        else if (verticalMovement < 0 && verticalMovement > -0.55f)
+        {
+            snappedVertical = -0.5f;
+        }
+        else if (verticalMovement < -0.5f)
+        {
+            snappedVertical = -1;
+        }
+        else
+        {
+            snappedVertical = 0;
+        }
+        #endregion
+
+        animator.SetFloat(horizontal,snappedHorizontal,0.1f,Time.deltaTime);
+        animator.SetFloat(vertical, snappedVertical, 0.1f, Time.deltaTime);
+    }
+}
